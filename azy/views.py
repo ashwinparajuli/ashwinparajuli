067d0910ca4ebc46a74a186fa6a056b709dfadfa
Ashwin <ashwinparajuli1903@gmail.com>
2024-03-12 00:12:05 +0545

    loop

--- azy/views.py
+++ azy/views.py
@@ -6,6 +6,7 @@ def homePage(request):
         'title':'Home Page',
         'body':'Hello World!',
         'list':['Ashwin', 'Azy', 'Azywin'],
+        'numbers':[10,20,30,40,50,60],
         'details':[
             {'Name':'Ashwin Parajuli','Phone':'9864374144'},
             {'Name':'Azy win','Phone':'061588945'},
